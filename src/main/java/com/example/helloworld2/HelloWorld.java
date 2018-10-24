85614cde7148a4259d90c29f6cd7e6fe4d377482
nigelol <nigelolayan@gmail.com>
2018-10-24 16:27:21 +0800

    Hello world 2 test on local branch

---
+++ src/main/java/com/example/helloworld2/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.helloworld2;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int i = 9;
+        System.out.printf("Hello, World %s", i);
+    }
+}
